b7867b02fb538b46d16d662f4671c21872276b2b
shre-afk <shrebhimraopatil@gmail.com>
2024-08-20 01:09:12 +0530

    Add files via upload
    Added KV Creation Code

--- main.tf
+++ main.tf
@@ -15,4 +15,22 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "trial-services-eastus-rg"
   location = "eastus"
+
+  tags = {
+    Environment = "Dev"
+    Purpose = "learning"
+    DeployedBy ="Shre"
+  }
+}
+
+resource "azurerm_key_vault" "kv" {
+    name = "trial-appstore-dev-kv"
+    location = "eastus"
+    resource_group_name = "trial-services-eastus-rg"
+    sku_name = "standard"
+    tenant_id = "63c70cdf-c355-461c-a4f4-b0d8f939fe0b"
+    tags = {
+        Environment = "Dev"
+        DeployedBy = "Shre"
+    }
 }
